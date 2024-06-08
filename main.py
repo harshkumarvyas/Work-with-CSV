ffb15e36354ddfb0321ba5254424e4e1dee99e5a
Harsh Vyas <harshvyas5527+github@gmail.com>
2024-06-08 20:53:28 +0530

    Learn some pandas methods

--- main.py
+++ main.py
@@ -11,4 +11,20 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+
+# data_dict = data.to_dict()
+# print(len(data_dict))
+
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+
+# print(data['temp'].max())
+
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+print(((monday.temp) * 1.8 ) + 32)
