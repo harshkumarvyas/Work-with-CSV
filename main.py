821b34d4554a8c353f5b9704836029144f0e6e1c
Harsh Vyas <harshvyas5527+github@gmail.com>
2024-06-08 20:19:59 +0530

    Simplify code by installing pandas library and having same output by writing only 3 lines of code and make output more readable.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
